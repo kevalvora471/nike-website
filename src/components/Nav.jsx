f05408686f959c40bee73f2f955f3d4056d6a6b9
vorakeval <vorakevin471@gmail.com>
2024-08-07 09:31:34 +0530

    make navbar responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { headerLogo } from '../assets/images'
-import { hamburger } from '../assets/icons'
+import { crossIcon, hamburger } from '../assets/icons'
 import { navLinks } from '../constants';
 
 const Nav = () => {
-  return (
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  return (<>
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
         <a href=""><img src={headerLogo} alt="Logo" width={130} height={29} /></a>
@@ -22,11 +27,27 @@ const Nav = () => {
           <span>/</span>
           <a href='/'>Explore now</a>
         </div>
-        <div className='hidden max-lg:block'>
-          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        <div className='hidden max-lg:block cursor-pointer' onClick={toggleMenu}>
+          {isMenuOpen 
+            ? 
+              <img src={crossIcon} className='font-bold' alt='hamburger icon' width={20} height={20} /> 
+            : <img src={hamburger} alt='hamburger icon' width={25} height={25} />}
         </div>
+
+
+
       </nav>
+      {
+        isMenuOpen ? (
+          <ul className='mt-3 flex-col lg:hidden rounded-xl bg-[#ff6452] opacity-95 font-montserrat border-2  flex justify-center items-center gap-5 p-4 z-50 transition-opacity duration-1000' >
+            <li><a href="#home" className='text-white px-6 py-2 hover:bg-slate-100 rounded-full hover:text-yellow-950 transition duration-150 ease-in-out'>Home</a></li>
+            <li><a href="#about-us" className='text-white px-6 py-2 hover:bg-slate-100 rounded-full hover:text-yellow-950 transition duration-150 ease-in-out'>About Us</a></li>
+            <li><a href="#products" className='text-white px-6 py-2 hover:bg-slate-100 rounded-full hover:text-yellow-950 transition duration-150 ease-in-out'>Products</a></li>
+            <li><a href="#contact-us" className='text-white px-6 py-2 hover:bg-slate-100 rounded-full hover:text-yellow-950 transition duration-150 ease-in-out'>Contact Us</a></li>
+          </ul >) : null}
     </header>
+
+  </>
   )
 }
 
