8f77221cd58295dcfbf512de937a55757a37b699
vorakeval <vorakevin471@gmail.com>
2024-08-07 17:19:38 +0530

    dd

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
           {navLinks.map((item) => (
             <li key={item.label}>
-              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
+              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray hover:text-coral-red">
                 {item.label}
               </a>
             </li>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
       </div>
 
 
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center z-0'>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center z-0' >
         <img src={bigShoeImg} alt="shoes collection" width={610} height={500} className='object-contain relative z-10' />
 
         <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
