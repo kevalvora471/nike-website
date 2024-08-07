b8ce5d915d151fcfa121e82b83c164eb16669e95
vorakeval <vorakevin471@gmail.com>
2024-08-07 17:07:08 +0530

    done done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
         
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold lg:z-50'>
           <span className='xl:bg-white xl:whitespace-nowrap relative  pr-10'>The New Arrival</span>
           <br />
           <span className='text-coral-red inline-block mt-3'>Nike </span> Shoes
@@ -40,7 +40,7 @@ const Hero = () => {
       </div>
 
 
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center z-0'>
         <img src={bigShoeImg} alt="shoes collection" width={610} height={500} className='object-contain relative z-10' />
 
         <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
