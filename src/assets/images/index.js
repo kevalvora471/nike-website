c4f0c38c97c4fdde8d77a8d6bdf3d5e4fcc94631
vorakeval <vorakevin471@gmail.com>
2024-08-07 18:24:16 +0530

    change to webp

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,8 @@
-import bigShoe1 from './big-shoe1.png'
-import bigShoe2 from './big-shoe2.png'
-import bigShoe3 from './big-shoe3.png'
+import bigShoe1 from './big-shoe1.webp'
+import bigShoe2 from './big-shoe2.webp'
+import bigShoe3 from './big-shoe3.webp'
 
-import customer1 from './customer1.jpeg'
+import customer1 from './customer1.webp'
 import customer2 from './customer2.svg'
 
 import footerLogo from './footer-logo.svg'
